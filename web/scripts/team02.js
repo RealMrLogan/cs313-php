fbb2798e168882c9a5f6b3d86fd98deac59e1180
Logan Saunders <lsaunders275@gmail.com>
2019-05-01 20:05:38 -0600

    finished core challenges

--- web/scripts/team02.js
+++ web/scripts/team02.js
@@ -2,6 +2,7 @@ document.getElementById("click-me").addEventListener("click", e => {
     alert("Clicked!");
 });
 
+// Vanilla JS
 document.getElementById("change-color").addEventListener("click", e => {
     const color = document.getElementById("new-color").value;
     document.getElementById("first-div").style.backgroundColor = color;
