36e22cab28492dcf168307e9a32cb32e9a2cc0e0
Logan Saunders <lsaunders275@gmail.com>
2019-05-01 19:59:43 -0600

    added function to change color by button press

--- web/scripts/team02.js
+++ web/scripts/team02.js
@@ -1,3 +1,8 @@
 document.getElementById("click-me").addEventListener("click", e => {
     alert("Clicked!");
 });
+
+document.getElementById("change-color").addEventListener("click", e => {
+    const color = document.getElementById("new-color").nodeValue;
+    document.getElementById("first-div").style.backgroundColor = color;
+});
