05970766f1b3c06be48048e73b18860945d5138e
Logan Saunders <lsaunders275@gmail.com>
2019-05-04 16:08:56 -0600

    ponder 02

--- web/assignments.php
+++ web/assignments.php
@@ -4,7 +4,7 @@
 <head>
    <meta charset="utf-8" />
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <title>Introduction</title>
+   <title>Assignments</title>
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <meta name="author" content="Logan Saunders">
    <meta name="description" content="">
@@ -12,18 +12,12 @@
 </head>
 
 <body>
-   <header>An Intro to Me</header>
+   <header>My Assignments</header>
    <?php include 'nav.php';?>
    <hr>
    <main>
-      <h1>My name is Logan Saunders</h1>
-      <div>
-         <img src="images/profile-picture.jpg" alt="Profile Picture" class="text-picture">
-         <p>
-            If you haven't guessed it by now, my name is Logan. I am interested in a few different things like hiking, spending time with my family and I also enjoy pop culture (video games and movies).<br /> <br />
-            
-         </p>
-      </div>
+      <h1>School Assignments</h1>
+      <p>coming soon...</p>
    </main>
    <footer>
       <p>&copy; Logan Saunders, <span id="date"></span></p>
---
+++ web/nav.php
@@ -0,0 +1,6 @@
+<?php
+    echo "<nav>
+    <a href='index.php' title='Intro'>Introduction</a>
+    <a href='assignments.php' title='Assignments'>Assignments</a>
+ </nav>"
+?>
