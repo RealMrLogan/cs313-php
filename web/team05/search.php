d4b6461252beeed2d33daa1e45de34699288f862
Logan Saunders <lsaunders275@gmail.com>
2019-05-21 21:51:25 -0600

    .

--- web/team05/search.php
+++ web/team05/search.php
@@ -1,3 +1,11 @@
 <?php
    var_dump($_POST);
+   $name = $_POST["name"];
+   echo "Name: " . $name;
+
+   foreach ($db->query('SELECT * FROM scriptures WHERE name=:book') as $row)
+   {
+      echo "Got a book!";
+      echo '<b>' . $row['book'] . ' </b>' . $row['chapter'] . ':' . $row['verse'] . ' - "' . $row['content'] . '"<br><br>';
+   }
 ?>
