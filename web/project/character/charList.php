99b6873158643cba59976edd7d601c109aeb4dc7
Logan Saunders <lsaunders275@gmail.com>
2019-05-25 17:32:15 -0600

    commit

--- web/project/character/charList.php
+++ web/project/character/charList.php
@@ -25,7 +25,6 @@
          require_once '../connections.php';
          foreach ($db->query('SELECT * FROM character') as $row)
          {
-            print_r($row);
             ?>
             <form action="charDetails.php" method="get">
                <input type="submit" name="displayname" value="<?php echo $row['displayname']; ?>">
