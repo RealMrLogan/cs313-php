579e9fcc4cbc7fe1cc574e43f02fc709c7a0e09b
Logan Saunders <lsaunders275@gmail.com>
2019-05-29 20:54:27 -0600

    commit

--- web/team06/index.php
+++ web/team06/index.php
@@ -22,14 +22,14 @@
             echo '<input type="checkbox" name="$row["topic_id"]">' . $row['name'];
          }
          ?>
-         <input type="submit" value="">
+         <input type="submit" value="Submit">
       </form>
       <?php
       // Get the database connection file
       require_once 'connections.php';
 
       if (isset($_POST)) {
-
+         $db->exec('INSERT INTO scripture(book, chapter, verse, content) VALUES($_POST["book"], $_POST["chapter"], $_POST["verse"], $_POST["content"])');
       }
       ?>
    </main>
