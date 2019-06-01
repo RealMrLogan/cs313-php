80dfd8fabcef88787ef22947b1859c794c70157d
Logan Saunders <lsaunders275@gmail.com>
2019-06-01 16:02:24 -0600

    debug

--- web/project/items/itemDetails.php
+++ web/project/items/itemDetails.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +8,8 @@
    <link rel="stylesheet" href="../styles.css">
    <title>Details | Game</title>
 </head>
-<body>   
+
+<body>
    <header>
       <h1>Item Details</h1>
       <nav>
@@ -19,53 +21,60 @@
    <main>
       <section class="charDetails">
          <?php
-            // Get the database connection file
-            require_once '../connections.php';
-            $stmnt = 0;
-            if (isset($_GET["weapon"])) {
-               $query = $_GET["weapon"];
-               $stmnt = $db->prepare('SELECT * FROM weapons WHERE displayname=:query');
-            } else if (isset($_GET["protection"])) {
-               $query = $_GET["protection"];
-               $stmnt = $db->prepare('SELECT * FROM protection WHERE displayname=:query');
-            } else if (isset($_GET["spell"])) {
-               $query = $_GET["spell"];
-               $stmnt = $db->prepare('SELECT * FROM spells WHERE displayname=:query');
-            }
-            $stmnt->bindValue(':query', $query, PDO::PARAM_STR);
-            $stmnt->execute();
-            $rows = $stmnt->fetchAll(PDO::FETCH_ASSOC);
+         // Get the database connection file
+         require_once '../connections.php';
+         $stmnt = 0;
+         $type = "";
+         if (isset($_GET["weapon"])) {
+            $query = $_GET["weapon"];
+            $stmnt = $db->prepare('SELECT * FROM weapons WHERE displayname=:query');
+            $type = "weapon";
+         } else if (isset($_GET["protection"])) {
+            $query = $_GET["protection"];
+            $stmnt = $db->prepare('SELECT * FROM protection WHERE displayname=:query');
+            $type = "protection";
+         } else if (isset($_GET["spell"])) {
+            $query = $_GET["spell"];
+            $stmnt = $db->prepare('SELECT * FROM spells WHERE displayname=:query');
+            $type = "spell";
+         }
+         $stmnt->bindValue(':query', $query, PDO::PARAM_STR);
+         $stmnt->execute();
+         $rows = $stmnt->fetchAll(PDO::FETCH_ASSOC);
 
-            if (isset($rows)) {
-               foreach ($rows as $row)
-               {
-                  var_dump($row);
-                  if ($row['weaponid']) {
+         if (isset($rows)) {
+            foreach ($rows as $row) {
+               switch ($type) {
+                  case "weapon":
                      ?>
-                     <label>Weapon: <?php echo $row['displayname']; ?></label> <br>
-                     <label>Damage: <?php echo $row['damage']; ?></label> <br>
-                     <label>Range: <?php echo $row['range']; ?></label> <br>
-                     <label>Durability: <?php echo $row['durability']; ?></label>
-                     <?php
-                  } else if ($row['protectionid']) {
-                     ?>
-                     <label>Protection: <?php echo $row['displayname']; ?></label> <br>
-                     <label>Armor: <?php echo $row['armor']; ?></label> <br>
-                     <label>Durability: <?php echo $row['durability']; ?></label>
-                     <?php
-                  } else if ($row['spellid']) {
-                     ?>
-                     <label>Spell: <?php echo $row['displayname']; ?></label> <br>
-                     <label>Damage: <?php echo $row['damage']; ?></label> <br>
-                     <label>Range: <?php echo $row['range']; ?></label> <br>
-                     <label>Cost: <?php echo $row['cost']; ?></label> <br>
-                     <label>Cooldown: <?php echo $row['cooldown']; ?></label>
-                     <?php
-                  }
-               }
+                  <label>Weapon: <?php echo $row['displayname']; ?></label> <br>
+                  <label>Damage: <?php echo $row['damage']; ?></label> <br>
+                  <label>Range: <?php echo $row['range']; ?></label> <br>
+                  <label>Durability: <?php echo $row['durability']; ?></label>
+                  <?php
+                  break;
+               case "protection":
+                  ?>
+                  <label>Protection: <?php echo $row['displayname']; ?></label> <br>
+                  <label>Armor: <?php echo $row['armor']; ?></label> <br>
+                  <label>Durability: <?php echo $row['durability']; ?></label>
+                  <?php
+                  break;
+               case "spell":
+                  ?>
+                  <label>Spell: <?php echo $row['displayname']; ?></label> <br>
+                  <label>Damage: <?php echo $row['damage']; ?></label> <br>
+                  <label>Range: <?php echo $row['range']; ?></label> <br>
+                  <label>Cost: <?php echo $row['cost']; ?></label> <br>
+                  <label>Cooldown: <?php echo $row['cooldown']; ?></label>
+                  <?php
+                  break;
             }
-         ?>
+         }
+      }
+      ?>
       </section>
    </main>
 </body>
+
 </html>
