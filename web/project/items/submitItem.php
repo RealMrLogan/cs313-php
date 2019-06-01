c3573f3fa76afad6fc294c41250c7e2946d06811
Logan Saunders <lsaunders275@gmail.com>
2019-06-01 15:55:19 -0600

    debug

--- web/project/items/submitItem.php
+++ web/project/items/submitItem.php
@@ -20,6 +20,36 @@ if (isset($_POST)) {
             $stmnt->bindValue(':durability', $dur, PDO::PARAM_STR);
             $stmnt->execute();
             break;
+         case "protection":
+            $name = filter_input(INPUT_POST, 'displayname', FILTER_SANITIZE_STRING);
+            // TODO: check for duplicate name
+            $armor = filter_input(INPUT_POST, 'armor', FILTER_SANITIZE_STRING);
+            $dur = filter_input(INPUT_POST, 'durability', FILTER_SANITIZE_STRING);
+
+            $stmnt = $db->prepare('INSERT INTO protection(displayname, armor, durability) 
+                                       VALUES(:name, :armor, :durability)');
+            $stmnt->bindValue(':name', $name, PDO::PARAM_STR);
+            $stmnt->bindValue(':armor', $armor, PDO::PARAM_STR);
+            $stmnt->bindValue(':durability', $dur, PDO::PARAM_STR);
+            $stmnt->execute();
+            break;
+         case "spell":
+            $name = filter_input(INPUT_POST, 'displayname', FILTER_SANITIZE_STRING);
+            // TODO: check for duplicate name
+            $damage = filter_input(INPUT_POST, 'damage', FILTER_SANITIZE_STRING);
+            $range = filter_input(INPUT_POST, 'range', FILTER_SANITIZE_STRING);
+            $cost = filter_input(INPUT_POST, 'cost', FILTER_SANITIZE_STRING);
+            $cooldown = filter_input(INPUT_POST, 'cooldown', FILTER_SANITIZE_STRING);
+
+            $stmnt = $db->prepare('INSERT INTO spells(displayname, damage, range, cost, cooldown) 
+                                       VALUES(:name, :damage, :range, :cost, :cooldown)');
+            $stmnt->bindValue(':name', $name, PDO::PARAM_STR);
+            $stmnt->bindValue(':damage', $damage, PDO::PARAM_STR);
+            $stmnt->bindValue(':range', $range, PDO::PARAM_STR);
+            $stmnt->bindValue(':cost', $cost, PDO::PARAM_STR);
+            $stmnt->bindValue(':cooldown', $cooldown, PDO::PARAM_STR);
+            $stmnt->execute();
+            break;
       }
 
 
