e11786dd8fde22c455ed3862c220681bb043ae0a
Logan Saunders <lsaunders275@gmail.com>
2019-06-01 13:48:22 -0600

    debug

--- web/project/character/submitChar.php
+++ web/project/character/submitChar.php
@@ -3,20 +3,20 @@
    require_once 'connections.php';
    var_dump($_POST);
          
-   if (isset($_POST)) {
-      $name = filter_input(INPUT_POST, 'displayname', FILTER_SANITIZE_STRING);
-      $hp = filter_input(INPUT_POST, 'hitpoints', FILTER_SANITIZE_STRING);
-      $damage = filter_input(INPUT_POST, 'damage', FILTER_SANITIZE_STRING);
-      $armor = filter_input(INPUT_POST, 'armor', FILTER_SANITIZE_STRING);
-      $magic = filter_input(INPUT_POST, 'magic', FILTER_SANITIZE_STRING);
+   // if (isset($_POST)) {
+   //    $name = filter_input(INPUT_POST, 'displayname', FILTER_SANITIZE_STRING);
+   //    $hp = filter_input(INPUT_POST, 'hitpoints', FILTER_SANITIZE_STRING);
+   //    $damage = filter_input(INPUT_POST, 'damage', FILTER_SANITIZE_STRING);
+   //    $armor = filter_input(INPUT_POST, 'armor', FILTER_SANITIZE_STRING);
+   //    $magic = filter_input(INPUT_POST, 'magic', FILTER_SANITIZE_STRING);
       
-      $stmnt = $db->prepare('INSERT INTO character(displayname, hitpoints, damage, armor, magic) 
-                              VALUES(:name, :hp, :damage, :armor, :magic)');
-      $stmnt->bindValue(':name', $name, PDO::PARAM_STR);
-      $stmnt->bindValue(':hp', $hp, PDO::PARAM_STR);
-      $stmnt->bindValue(':damage', $damage, PDO::PARAM_STR);
-      $stmnt->bindValue(':armor', $armor, PDO::PARAM_STR);
-      $stmnt->bindValue(':magic', $magic, PDO::PARAM_STR);
-      $stmnt->execute();
-   }
+   //    $stmnt = $db->prepare('INSERT INTO character(displayname, hitpoints, damage, armor, magic) 
+   //                            VALUES(:name, :hp, :damage, :armor, :magic)');
+   //    $stmnt->bindValue(':name', $name, PDO::PARAM_STR);
+   //    $stmnt->bindValue(':hp', $hp, PDO::PARAM_STR);
+   //    $stmnt->bindValue(':damage', $damage, PDO::PARAM_STR);
+   //    $stmnt->bindValue(':armor', $armor, PDO::PARAM_STR);
+   //    $stmnt->bindValue(':magic', $magic, PDO::PARAM_STR);
+   //    $stmnt->execute();
+   // }
 ?>
