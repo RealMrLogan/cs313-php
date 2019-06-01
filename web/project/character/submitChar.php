9a19d8cb8e847ba3b1d656c2faeb7d9ab26bbd02
Logan Saunders <lsaunders275@gmail.com>
2019-06-01 13:57:18 -0600

    debug

--- web/project/character/submitChar.php
+++ web/project/character/submitChar.php
@@ -10,8 +10,8 @@
       $armor = filter_input(INPUT_POST, 'armor', FILTER_SANITIZE_STRING);
       $magic = filter_input(INPUT_POST, 'magic', FILTER_SANITIZE_STRING);
       
-      $stmnt = $db->prepare('INSERT INTO character(displayname, hitpoints, damage, armor, magic) 
-                              VALUES(:name, :hp, :damage, :armor, :magic)');
+      $stmnt = $db->prepare('INSERT INTO character(displayname, hitpoints, damage, armor, magic, isdead) 
+                              VALUES(:name, :hp, :damage, :armor, :magic, f)');
       $stmnt->bindValue(':name', $name, PDO::PARAM_STR);
       $stmnt->bindValue(':hp', $hp, PDO::PARAM_STR);
       $stmnt->bindValue(':damage', $damage, PDO::PARAM_STR);
