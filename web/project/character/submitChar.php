2cddfd2aee35f69a969199297fc8154a89ec48fa
Logan Saunders <lsaunders275@gmail.com>
2019-06-01 14:03:44 -0600

    debug

--- web/project/character/submitChar.php
+++ web/project/character/submitChar.php
@@ -1,23 +1,30 @@
 <?php
    // Get the database connection file
    require_once '../connections.php';
-   var_dump($_POST);
          
    if (isset($_POST)) {
-      $name = filter_input(INPUT_POST, 'displayname', FILTER_SANITIZE_STRING);
-      $hp = filter_input(INPUT_POST, 'hitpoints', FILTER_SANITIZE_STRING);
-      $damage = filter_input(INPUT_POST, 'damage', FILTER_SANITIZE_STRING);
-      $armor = filter_input(INPUT_POST, 'armor', FILTER_SANITIZE_STRING);
-      $magic = filter_input(INPUT_POST, 'magic', FILTER_SANITIZE_STRING);
-      
-      $stmnt = $db->prepare('INSERT INTO character(displayname, hitpoints, damage, armor, magic, isdead) 
-                              VALUES(:name, :hp, :damage, :armor, :magic, :f)');
-      $stmnt->bindValue(':name', $name, PDO::PARAM_STR);
-      $stmnt->bindValue(':hp', $hp, PDO::PARAM_STR);
-      $stmnt->bindValue(':damage', $damage, PDO::PARAM_STR);
-      $stmnt->bindValue(':armor', $armor, PDO::PARAM_STR);
-      $stmnt->bindValue(':magic', $magic, PDO::PARAM_STR);
-      $stmnt->bindValue(':magic', 'f', PDO::PARAM_STR);
-      $stmnt->execute();
+      try {
+         $name = filter_input(INPUT_POST, 'displayname', FILTER_SANITIZE_STRING);
+         $hp = filter_input(INPUT_POST, 'hitpoints', FILTER_SANITIZE_STRING);
+         $damage = filter_input(INPUT_POST, 'damage', FILTER_SANITIZE_STRING);
+         $armor = filter_input(INPUT_POST, 'armor', FILTER_SANITIZE_STRING);
+         $magic = filter_input(INPUT_POST, 'magic', FILTER_SANITIZE_STRING);
+         $notDead = false;
+         
+         $stmnt = $db->prepare('INSERT INTO character(displayname, hitpoints, damage, armor, magic, isdead) 
+                                 VALUES(:name, :hp, :damage, :armor, :magic, :notdead)');
+         $stmnt->bindValue(':name', $name, PDO::PARAM_STR);
+         $stmnt->bindValue(':hp', $hp, PDO::PARAM_STR);
+         $stmnt->bindValue(':damage', $damage, PDO::PARAM_STR);
+         $stmnt->bindValue(':armor', $armor, PDO::PARAM_STR);
+         $stmnt->bindValue(':magic', $magic, PDO::PARAM_STR);
+         $stmnt->bindValue(':notdead', $notDead, PDO::PARAM_STR);
+         $stmnt->execute();
+
+         echo "Created $name!"
+      } catch (Exception $e) {
+         echo 'Error when trying to add the character';
+         echo 'Message: ' .$e->getMessage();
+      }
    }
 ?>
