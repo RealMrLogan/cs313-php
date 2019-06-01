b39d27083bb4ea9735fb755e1a5d3046a4eb4f86
Logan Saunders <lsaunders275@gmail.com>
2019-06-01 16:16:01 -0600

    debug

---
+++ web/project/items/itemSearch.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <link rel="stylesheet" href="../styles.css">
+   <title>Item | Game</title>
+</head>
+
+<body>
+   <header>
+      <h1>Item Search</h1>
+      <nav>
+         <a href="../index.html">&larr; Home</a>
+      </nav>
+   </header>
+   <hr>
+   <main>
+      <form action="itemSearch.php" method="post">
+         <fieldset>
+            <legend>Which category?</legend>
+            <input type="radio" name="weapon"> <label>Weapon</label>
+            <input type="radio" name="protection"> <label>Protection</label>
+            <input type="radio" name="spell"> <label>Spell</label>
+         </fieldset>
+         <label for="">Which Item: </label><input type="text" name="search-query">
+         <input type="submit" value="Search">
+      </form>
+      <hr>
+      <?php
+         // Get the database connection file
+         require_once '../connections.php';
+         $query = filter_input(INPUT_POST, 'search-query', FILTER_SANITIZE_STRING);
+         $type = "";
+         if (isset($_POST['weapon'])) { $type = "weapons"; }
+         else if (isset($_POST['protection'])) { $type = "protection"; }
+         else if (isset($_POST['spell'])) { $type = "spells"; }
+
+         $stmt = $db->prepare("SELECT * FROM $type WHERE displayname=:displayname");
+         $stmt->bindValue(':displayname', $query, PDO::PARAM_STR);
+         $stmt->execute();
+         $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+         if (isset($rows)) {
+            ?>
+            <h2><?php echo count($rows); ?> Result(s)</h2>
+            <ul>
+            <?php
+            foreach ($rows as $row)
+            {
+               ?>
+               <li><?php echo $row['displayname']; ?></li>
+               <?php
+            }
+            ?>
+            </ul>
+            <?php
+         } else {
+            ?>
+            <h2>No Results...</h2>
+            <?php
+         }
+         ?>
+      <section class="options">
+         
+      </section>
+   </main>
+</body>
+
+</html>
