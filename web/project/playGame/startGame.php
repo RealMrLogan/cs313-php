ff9324ecb0a000af035edfa8573910d69f80a0dc
Logan Saunders <lsaunders275@gmail.com>
2019-06-08 13:02:38 -0600

    debug Project 01

--- web/project/playGame/startGame.php
+++ web/project/playGame/startGame.php
@@ -36,33 +36,34 @@ sendToJS($_SESSION);
       <section class="options" id="player-options">
          <button onclick="playerAttack()">Attack</button>
       </section>
-      <section class="stats-section">
-         <div class="character-stats">
-            <h3 id="player-name"></h3>
-            <div>
-               <h4>Hitpoints: </h4><label class="stat-label" id="player-hp"></label>
-            </div>
-            <div>
-               <h4>Equipped Weapon: </h4><label class="stat-label" id="player-weapon"></label>
-            </div>
-            <div>
-               <h4>Equipped Protection: </h4><label class="stat-label" id="player-protection"></label>
-            </div>
-         </div>
-         <div class="character-stats">
-            <h3 id="opponent-name"></h3>
-            <div>
-               <h4>Hitpoints: </h4><label class="stat-label" id="opponent-hp"></label>
-            </div>
-            <div>
-               <h4>Equipped Weapon: </h4><label class="stat-label" id="opponent-weapon"></label>
-            </div>
-            <div>
-               <h4>Equipped Protection: </h4><label class="stat-label" id="opponent-protection"></label>
-            </div>
-         </div>
-      </section>
    </main>
+   
+   <section class="stats-section">
+      <div class="character-stats">
+         <h3 id="player-name"></h3>
+         <div>
+            <h4>Hitpoints: </h4><label class="stat-label" id="player-hp"></label>
+         </div>
+         <div>
+            <h4>Equipped Weapon: </h4><label class="stat-label" id="player-weapon"></label>
+         </div>
+         <div>
+            <h4>Equipped Protection: </h4><label class="stat-label" id="player-protection"></label>
+         </div>
+      </div>
+      <div class="character-stats">
+         <h3 id="opponent-name"></h3>
+         <div>
+            <h4>Hitpoints: </h4><label class="stat-label" id="opponent-hp"></label>
+         </div>
+         <div>
+            <h4>Equipped Weapon: </h4><label class="stat-label" id="opponent-weapon"></label>
+         </div>
+         <div>
+            <h4>Equipped Protection: </h4><label class="stat-label" id="opponent-protection"></label>
+         </div>
+      </div>
+   </section>
    <script src="startGame.js"></script>
 </body>
 
